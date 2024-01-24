4e2b4faef7c9f8f12525bf6c2f911f5844c926bf
1nonlyabhi <gabhishek0407@gmail.com>
2024-01-24 23:51:52 +0530

    change name header

--- script.js
+++ script.js
@@ -22,18 +22,22 @@ for (var i=0; themeDots.length > i; i++){
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('name').src = "https://capsule-render.vercel.app/api?type=venom&height=200&text=abhishek%20kr.%20gupta&fontSize=70&color=0:8871e5,100:b678c4&stroke=b678c4&desc=full-stack%20web%20developer,%20B'lore%20India%20🇮🇳&descAlignY=73&descAlign=61.5"
 	}
 
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('name').src = "https://capsule-render.vercel.app/api?type=venom&height=200&text=abhishek%20kr.%20gupta&fontSize=70&fontColor=f3f6f4&color=0:8871e5,100:b678c4&stroke=b678c4&desc=full-stack%20web%20developer,%20B'lore%20India%20🇮🇳&descAlignY=73&descAlign=61.5"
 	}
 
 	if(mode == 'green'){
 		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById('name').src = "https://capsule-render.vercel.app/api?type=transparent&height=200&text=abhishek%20kr.%20gupta&fontSize=70&fontColor=f3f6f4&stroke=83BE25&desc=full-stack%20web%20developer,%20B'lore%20India%20🇮🇳&descAlignY=73&descAlign=61.5"
 	}
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById('name').src = "https://capsule-render.vercel.app/api?type=transparent&height=200&text=abhishek%20kr.%20gupta&fontSize=70&fontColor=f3f6f4&stroke=b678c4&desc=full-stack%20web%20developer,%20B'lore%20India%20🇮🇳&descAlignY=73&descAlign=61.5"
 	}
 
 	localStorage.setItem('theme', mode)
